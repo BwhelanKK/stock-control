8b664c2138518027b2248b3bdd75bd6de5bda854
Brendan Whelan <whelan_brendan@hotmail.com>
2024-02-10 22:28:31 +0000

    Added user input for item quantity for cycle count

--- run.py
+++ run.py
@@ -19,11 +19,26 @@ def count_stock_items():
     User can not enter either a quanity higher than this or zero
     """
     print("Welcome to the stock control system.\n")
-    print("Enter the number of stock items that you'd like for your cycle count.\n")
     print("The current number of stock items are:\n")
     stock = SHEET.worksheet("stock").col_values(1)
-    print(len(stock))
+    print(len(stock)-1)
+    print("Enter the number of stock items that you'd like for your cycle count.\n")
+    print("The number has to be equal to or lower than current stock quantity above:\n")
+
+    count_qty = input("Please enter the number of stock items required for the cycle count:")
+
+    print(count_qty)
+
     print("********************************************\n")
+
+
+def get_cycle_count_qty():
+    """
+    Get the number of items for cycle counting from the user
+    The number has to be greater than zero and equal to or less than 
+    the quanity of stock items currently held
+    """
+    print
 
 
 
