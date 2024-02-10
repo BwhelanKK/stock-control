9aed56054c2bffa4060f3de124c41d7fd5f762d2
Brendan Whelan <whelan_brendan@hotmail.com>
2024-02-10 21:57:38 +0000

    Tidied up the welcome method

--- run.py
+++ run.py
@@ -18,15 +18,22 @@ def count_stock_items():
     This is outputted to the user.
     User can not enter either a quanity higher than this or zero
     """
-    stock_qty = SHEET.row_count()
-    return stock_qty
+    print("Welcome to the stock control system.\n")
+    print("Enter the number of stock items that you'd like for your cycle count.\n")
+    print("The current number of stock items are:\n")
+    stock = SHEET.worksheet("stock").col_values(1)
+    print(len(stock))
+    print("********************************************\n")
+
+
+
 
 def main():
     """ 
     Run all functions
     """
+    count_stock_items()
 
-print("Welcome to Stock Count.")
 
 
 main()
